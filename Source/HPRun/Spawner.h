6f380756b839afc0b0ea17019ab9bacffb8e9189
hkenzo <heitorkenzo.k@gmail.com>
2019-06-11 19:01:56 -0300

    add include spawner

--- Source/HPRun/Spawner.h
+++ Source/HPRun/Spawner.h
@@ -1,27 +1,25 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
 #include "CoreMinimal.h"
+#include "Enemy.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Runtime/Core/Public/Math/UnrealMathUtility.h"
+#include "Components/TextRenderComponent.h"
+#include "Runtime/Engine/Public/TimerManager.h"
 #include "GameFramework/Actor.h"
-
 #include "Spawner.generated.h"
-
 UCLASS()
 class HPRUN_API ASpawner : public AActor
 {
 	GENERATED_BODY()
-	
 public:	
 	// Sets default values for this actor's properties
 	ASpawner();
-
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
 };
